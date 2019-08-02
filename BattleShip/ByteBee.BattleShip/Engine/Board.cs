32bd37e02377cc89c0bb74d3508ccd8c3f7a9347
Sergej Kukshaus <kukshaus@dial.de>
2019-08-03 00:34:42 +0200

    Schon einmal das Feld für Schiffe Versenken aufgebaut

---
+++ BattleShip/ByteBee.BattleShip/Engine/Board.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace ByteBee.BattleShip.Engine
+{
+    public abstract class Board : IEnumerable<Field>
+    {
+        private readonly Field[,] _memory = new Field[10, 10];
+        public Field this[int x, int y]
+        {
+            get => _memory[x, y];
+            protected set => _memory[x, y] = value;
+        }
+
+        public IList<Ship> Fleet { get; set; } = new List<Ship>();
+        public IEnumerator<Field> GetEnumerator()
+        {
+            return _memory.Cast<Field>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class PlayerBoard : Board
+    {
+
+    }
+
+    public class ComputerBoard : Board
+    {
+        public ComputerBoard()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    this[x, y] = new Field(x, y);
+                }
+            }
+        }
+
+        public void Generate()
+        {
+            var carrier = new Carrier(this);
+
+            for (int i = 0; i < carrier.SizeOfTheShip; i++)
+            {
+                this[3, i+3].IsShip = true;
+            }
+
+            Fleet.Add(carrier);
+        }
+    }
+
+    public class Field
+    {
+        public bool IsShip { get; set; }
+        public Point Coordinate { get; private set; }
+
+        public Field(int x, int y)
+        {
+            Coordinate = new Point(x, y);
+            
+        }
+    }
+
+    public abstract class Ship
+    {
+        public abstract int SizeOfTheShip { get; }
+        public Board Board { get; }
+
+        protected Ship(Board board)
+        {
+            Board = board;
+        }
+    }
+
+    public class Carrier : Ship
+    {
+        public override int SizeOfTheShip { get; } = 5;
+
+        public Carrier(Board board) : base(board)
+        {
+            
+        }
+    }
+
+    public class Battleship : Ship
+    {
+        public override int SizeOfTheShip { get; } = 4;
+        public Battleship(Board board) : base(board)
+        {
+        }
+    }
+
+    public class Cruiser : Ship
+    {
+        public override int SizeOfTheShip { get; } = 3;
+        public Cruiser(Board board) : base(board)
+        {
+        }
+    }
+
+    public class Destroyer : Ship
+    {
+        public override int SizeOfTheShip { get; } = 2;
+        public Destroyer(Board board) : base(board)
+        {
+        }
+    }
+}
---
+++ BattleShip/ByteBee.BattleShip/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ByteBee.BattleShip.Engine;
+
+namespace ByteBee.BattleShip
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ComputerBoard _computerBoard;
+        private int _currentRound;
+
+        public ComputerBoard ComputerBoard
+        {
+            get => _computerBoard;
+            set
+            {
+                _computerBoard = value;
+                OnPropertyChanged(nameof(ComputerBoard));
+            }
+        }
+
+        public string[] Numbers { get; } = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+        public string[] Letters { get; } = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+        public int CurrentRound
+        {
+            get => _currentRound;
+            set
+            {
+                _currentRound = value;
+                OnPropertyChanged(nameof(CurrentRound));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            ComputerBoard = new ComputerBoard();
+            ComputerBoard.Generate();
+
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ComputerFieldClicked(object sender, MouseButtonEventArgs e)
+        {
+            Field field = (Field) ((Panel) sender).DataContext;
+
+            CurrentRound++;
+        }
+    }
+}
